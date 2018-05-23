d68bb0a1612e5eb3111116951493f57f1e0d692b
Changbeom Choi <cbchoi@handong.edu>
2018-05-23 21:47:45 +1000

    update brand

---
+++ public/javascripts/qrcodegen-live.js
@@ -0,0 +1,63 @@
+/* 
+ * QR Code generator demo (JavaScript)
+ * 
+ * Copyright (c) 2016 Project Nayuki. (MIT License)
+ * https://www.nayuki.io/page/qr-code-generator-library
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ * - The above copyright notice and this permission notice shall be included in
+ *   all copies or substantial portions of the Software.
+ * - The Software is provided "as is", without warranty of any kind, express or
+ *   implied, including but not limited to the warranties of merchantability,
+ *   fitness for a particular purpose and noninfringement. In no event shall the
+ *   authors or copyright holders be liable for any claim, damages or other
+ *   liability, whether in an action of contract, tort or otherwise, arising from,
+ *   out of or in connection with the Software or the use or other dealings in the
+ *   Software.
+ */
+
+"use strict";
+
+
+function redrawQrCode() {
+	// Show/hide rows based on bitmap/vector image output
+	var bitmapOutput = true;
+
+	// Reset output images in case of early termination
+	var canvas = document.getElementById("qrcode-canvas");
+	//var svg = document.getElementById("qrcode-svg");
+	canvas.style.display = "none";
+
+	
+	// Get form inputs and compute QR Code
+	var ecl = qrcodegen.QrCode.Ecc.HIGH;
+	var text = document.getElementById("text-input").value;
+	var segs = qrcodegen.QrSegment.makeSegments(text);
+
+	var minVer = 1;
+	var maxVer = 40;
+	var mask = -1;
+	var boostEcc = true;
+	var qr = qrcodegen.QrCode.encodeSegments(segs, ecl, minVer, maxVer, mask, boostEcc);
+	
+	// Draw image output
+	var border = 4;
+	if (border < 0 || border > 100)
+		return;
+    var scale = 8;
+    if (scale <= 0 || scale > 30)
+        return;
+
+    qr.drawCanvas(scale, border, canvas);
+    canvas.style.removeProperty("display");
+
+}
+
+
+
+redrawQrCode();
--- routes/admin-village.js
+++ routes/admin-village.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 var Village = require('../models/village');
 var VRItem = require('../models/virtual_reality')
+var fs = require('fs')
+
+var PythonShell = require('python-shell');
 
 router.get('/', function (req, res) {
     Village.find(function (err, villages) {
@@ -113,6 +116,42 @@ router.delete('/:id', function (req, res) {
 router.get('/add_vr/:id', function (req, res) {
     console.log(req.params.id);
     res.render('vr-add', {community_id:req.params.id});
+});
+
+router.get('/brand_making/:id', function (req, res) {
+    console.log(req.params.id);
+    res.render('brand-making', {community_id:req.params.id});
+});
+
+router.post('/update_brand/', function (req, res) {
+    console.log(req.body.id);
+
+    // Camera Control
+    var matches = req.body.img.match(/^data:.+\/(.+);base64,(.*)$/);
+    var buffer = new Buffer(matches[2], 'base64');
+    var savePath = '../public/images/villages/' + req.body.id + '.jpg';
+    var outputPath = '../public/images/brands/' + req.body.id + '.jpg';
+    fs.writeFileSync(savePath, buffer);
+
+    // Generate QR Code
+    var options = { pythonPath: 'python3', args:[req.body.qr_text, savePath, outputPath, 250]};
+    PythonShell.run('../public/python/qrgen.py', options, function (err, results) {
+        if (err) throw err;
+        console.log('finished');
+    });
+
+    // Update Brand Image
+    Village.findOne({_id: req.body.id}, function (err, village) {
+        if (err) return res.status(500).json({ error: err });
+        if (!village) return res.status(404).json({ error: 'village not found' });
+        village.brand_image = '../images/brands/'+ req.body.id + '.jpg';
+
+        village.save(function(err){
+            if(err) res.status(500).json({error: 'failed to update'});
+            res.json({message: 'book updated'});
+        });
+    })
+    //res.redirect('/admin/villages');
 });
 
 router.get('/:id', function (req, res) {
